150ac39f510396590c21b39794d8d0d7edd9fe6d
Max <max.tributino2018@gmail.com>
2026-03-12 21:32:52 -0300

    quinto projeto

---
+++ Projeto5/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int valor1;
+            int total;
+
+            Console.WriteLine("Digite o valor");
+            valor1 = int.Parse(Console.ReadLine());
+            total = valor1 * 2;
+            Console.WriteLine("O dobro deste numero é "+total);
+        }
+    }
+}
