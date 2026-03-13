85ae0f1a6144d7d20b6bebdc58aac249b01ee67a
unknown <lab2084@etecjk.local>
2026-03-12 21:26:35 -0300

    primeiro projeto

---
+++ projeto1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome;
+
+            Console.WriteLine("Digite seu nome: ");
+            nome = Console.ReadLine();
+            Console.WriteLine("Olá, " + nome +"!");
+        }
+    }
+}
