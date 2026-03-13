7a7d0118475752042d6b06fcba9c35aa6a9d59e3
Max <max.tributino2018@gmail.com>
2026-03-12 21:32:03 -0300

    quarto projeto

---
+++ Projeto4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int C;
+            float F;
+
+            Console.WriteLine("Digite o grau em Celsius");
+            C = int.Parse(Console.ReadLine());
+            F = C * 9 / 5 + 32;
+            Console.WriteLine("A conversão para Fahrenheit é "+F);
+            
+            
+        }
+    }
+}
