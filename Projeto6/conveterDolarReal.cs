0a8eb4af41b418a7fd0839bd254ba75b07b325b2
Max <max.tributino2018@gmail.com>
2026-03-12 21:33:14 -0300

    sexto projeto

---
+++ Projeto6/conveterDolarReal.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto6
+{
+    internal class conveterDolarReal
+    {
+        static void Main(string[] args)
+        {
+            double d;
+            double r;
+
+            Console.WriteLine("Coloque o valor em dólar");
+            d = double.Parse(Console.ReadLine());
+            r = d * 5.2;
+            Console.WriteLine("Este valor em reais é " + r);
+        }
+    }
+}
